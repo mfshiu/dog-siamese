2fb453975a10c83d4ff573a3f93081bc8c05ef75
mfshiu <avatar.xu@gmail.com>
2020-12-31 03:59:09 +0800

    Add dogs

--- config.py
+++ config.py
@@ -1,8 +1,22 @@
 class Config:
     training_dir = "./data/dogs/training/"
     testing_dir = "./data/dogs/testing/"
-    # training_dir = "./data/faces/training/"
-    # testing_dir = "./data/faces/testing/"
+    train_batch_size = 64
+    train_number_epochs = 100
+
+    class Evaluate:
+        model_path = "trained/DogSiamese-gpu.pkl"
+        inference_output_path = "output/inferences.tsv"
+        eer_output_path = "output/eer.tsv"
+        dog_input_root = "data/verification"
+        # dog_input_root = "data/dogs/testing"
+        dog_count = 85
+        group_size = 2
+
+
+class FaceConfig:
+    training_dir = "./data/faces/training/"
+    testing_dir = "./data/faces/testing/"
     train_batch_size = 64
     train_number_epochs = 100
 
