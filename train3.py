c379918f3ad91059c6cfc84310b368813949574d
mfshiu <avatar.xu@gmail.com>
2021-03-06 17:03:27 +0800

    RandomCrop

--- train3.py
+++ train3.py
@@ -105,7 +105,7 @@ def train(model_path):
     siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
                                             transform=transforms.Compose([
                                                 # transforms.CenterCrop(image_size * 2),
-                                                # transforms.Resize((image_size, image_size)),
+                                                transforms.Resize((image_size*1.5, image_size*1.5)),
                                                 transforms.RandomCrop(image_size),
                                                 # transforms.ColorJitter(
                                                 #     brightness=0.05, contrast=0.05,
