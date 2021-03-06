c1985c4f6af5cdd505e4cf3a57d97decbcec1ea5
mfshiu <avatar.xu@gmail.com>
2021-03-06 23:41:34 +0800

    image_size

--- train3.py
+++ train3.py
@@ -20,7 +20,7 @@ from model import SiameseNetwork, ContrastiveLoss
 global use_gpu
 use_gpu = False
 
-image_size = 200
+image_size = 100
 
 trained_dir = "trained"
 if not os.path.exists(trained_dir):
