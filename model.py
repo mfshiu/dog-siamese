c0b985238ada712947423d3aee9c2ef1bb68d70e
mfshiu <avatar.xu@gmail.com>
2021-03-06 21:30:16 +0800

    dropout

--- model.py
+++ model.py
@@ -10,30 +10,30 @@ class SiameseNetwork(nn.Module):
         self.cnn1 = nn.Sequential(
             nn.ReflectionPad2d(1),
             nn.Conv2d(1, 4, kernel_size=3),
-            nn.Dropout(0.2),
+            nn.Dropout(0.15),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(4),
 
             nn.ReflectionPad2d(1),
             nn.Conv2d(4, 8, kernel_size=3),
-            nn.Dropout(0.2),
+            nn.Dropout(0.15),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(8),
 
             nn.ReflectionPad2d(1),
             nn.Conv2d(8, 8, kernel_size=3),
-            nn.Dropout(0.2),
+            nn.Dropout(0.15),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(8),
         )
 
         self.fc1 = nn.Sequential(
             nn.Linear(8 * image_size * image_size, 500),
-            nn.Dropout(0.2),
+            nn.Dropout(0.15),
             nn.ReLU(inplace=True),
 
             nn.Linear(500, 500),
-            nn.Dropout(0.2),
+            nn.Dropout(0.15),
             nn.ReLU(inplace=True),
 
             nn.Linear(500, 5))
