c217683e517c048fad84d8409e70d19c957bc536
mfshiu <avatar.xu@gmail.com>
2021-03-09 02:34:58 +0800

    find_dog

--- exam.py
+++ exam.py
@@ -46,9 +46,9 @@ def find_dog(img_path):
     for dog_id in dog_ids:
         similarities = exam_dog(dog_id, img_path)[1]
         similarities.sort()
-        # avg = sum(similarities) / len(similarities)
+        avg = sum(similarities) / len(similarities)
         # avg = sum(similarities[Config.exam_count//2:]) / (len(similarities)-Config.exam_count//2)
-        avg = sum(x**2 for x in similarities)**0.5
+        # avg = sum(x**2 for x in similarities)**0.5
         # avg = max(similarities[])
         log_lines.append("%s->%s(avg:%s,%s)\n" % (img_path, dog_id, avg, similarities))
         if avg > threshold and max_avg < avg:
